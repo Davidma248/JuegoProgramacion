e303bcb8fe6565ecf18821d24bf150ff152e648c
Davidma248 <93098655+Davidma248@users.noreply.github.com>
2021-11-04 10:07:42 -0500

    Add files via upload

---
+++ AUDIOMANAGER.cs
@@ -0,0 +1,21 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+
+public class AUDIOMANAGER : MonoBehaviour
+{
+    public SOUND[] sounds;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
